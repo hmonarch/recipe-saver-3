9fe5edf74578a69d3c215c57e162f744776b86f3
doctafaustus <clowd150@gmail.com>
2019-04-22 14:50:51 -0500

    Add styles to menu bar

--- client/src/components/DetailPanel.vue
+++ client/src/components/DetailPanel.vue
@@ -1,39 +1,41 @@
 <template>
   <div id="detail-panel" class="panel">
-    <editor-menu-bar :editor="editor">
-      <div slot-scope="{ commands, isActive }" class="menubar">
+    <div id="description">
+      <editor-menu-bar :editor="editor">
+        <div slot-scope="{ commands, isActive }" class="menubar">
 
-        <button class="menubar__button" :class="{ 'is-active': isActive.bold() }" @click="commands.bold">
-          <icon name="bold"/>
-        </button>
-        <button class="menubar__button" :class="{ 'is-active': isActive.italic() }" @click="commands.italic">
-          <icon name="italic"/>
-        </button>
-        <button class="menubar__button" :class="{ 'is-active': isActive.strike() }" @click="commands.strike">
-          <icon name="strike"/>
-        </button>
-        <button class="menubar__button" :class="{ 'is-active': isActive.underline() }" @click="commands.underline">
-          <icon name="underline"/>
-        </button>
-        <button class="menubar__button" :class="{ 'is-active': isActive.bullet_list() }" @click="commands.bullet_list">
-          <icon name="ul"/>
-        </button>
-        <button class="menubar__button" :class="{ 'is-active': isActive.ordered_list() }" @click="commands.ordered_list">
-          <icon name="ol"/>
-        </button>
-        <button class="menubar__button" @click="commands.horizontal_rule">
-          <icon name="hr"/>
-        </button>
-        <button class="menubar__button" @click="commands.undo">
-          <icon name="undo"/>
-        </button>
-        <button class="menubar__button" @click="commands.redo">
-          <icon name="redo"/>
-        </button>
+          <button class="menubar__button" :class="{ 'is-active': isActive.bold() }" @click="commands.bold">
+            <icon name="bold"/>
+          </button>
+          <button class="menubar__button" :class="{ 'is-active': isActive.italic() }" @click="commands.italic">
+            <icon name="italic"/>
+          </button>
+          <button class="menubar__button" :class="{ 'is-active': isActive.strike() }" @click="commands.strike">
+            <icon name="strike"/>
+          </button>
+          <button class="menubar__button" :class="{ 'is-active': isActive.underline() }" @click="commands.underline">
+            <icon name="underline"/>
+          </button>
+          <button class="menubar__button" :class="{ 'is-active': isActive.bullet_list() }" @click="commands.bullet_list">
+            <icon name="ul"/>
+          </button>
+          <button class="menubar__button" :class="{ 'is-active': isActive.ordered_list() }" @click="commands.ordered_list">
+            <icon name="ol"/>
+          </button>
+          <button class="menubar__button" @click="commands.horizontal_rule">
+            <icon name="hr"/>
+          </button>
+          <button class="menubar__button" @click="commands.undo">
+            <icon name="undo"/>
+          </button>
+          <button class="menubar__button" @click="commands.redo">
+            <icon name="redo"/>
+          </button>
 
-      </div>
-    </editor-menu-bar>
-    <editor-content :editor="editor" id="editor"></editor-content>
+        </div>
+      </editor-menu-bar>
+      <editor-content :editor="editor" id="editor"></editor-content>
+    </div>
   </div>
 </template>
 
@@ -115,49 +117,57 @@ export default {
   margin-right: auto;
   padding: 10px;
 
-  #editor {
-    
-    ol,
-    ul {
-      padding-left: 40px;
-      margin: 15px 0;
-      list-style-type: initial;
+  #description {
+    border: solid 1px #dadada;
+
+    .menubar {
+      padding: 10px 0 5px ;
+      border-bottom: solid 1px #dadada;
+      margin-bottom: 5px;
+
+      .menubar__button {
+        font-weight: 700;
+        display: -webkit-inline-box;
+        display: -ms-inline-flexbox;
+        display: inline-flex;
+        background: rgba(0,0,0,0);
+        border: 0;
+        color: #000;
+        padding: .2rem .5rem;
+        margin-right: .2rem;
+        border-radius: 3px;
+        cursor: pointer;
+
+        &.is-active {
+          background-color: rgba(0,0,0,.1);
+        }
+      }
     }
 
-    ol {
-      list-style-type: decimal;
-    }
+    #editor {
+      > div {
+        padding: 10px;
+      }
 
-    strong {
-      font-weight: 600;
-    }
+      ol,
+      ul {
+        padding-left: 40px;
+        margin: 15px 0;
+        list-style-type: initial;
+      }
 
-    em {
-      font-style: italic;
-    }
+      ol {
+        list-style-type: decimal;
+      }
 
-  }
-}
+      strong {
+        font-weight: 600;
+      }
 
+      em {
+        font-style: italic;
+      }
 
-.menubar {
-  margin-bottom: 25px;
-
-  .menubar__button {
-    font-weight: 700;
-    display: -webkit-inline-box;
-    display: -ms-inline-flexbox;
-    display: inline-flex;
-    background: rgba(0,0,0,0);
-    border: 0;
-    color: #000;
-    padding: .2rem .5rem;
-    margin-right: .2rem;
-    border-radius: 3px;
-    cursor: pointer;
-
-    &.is-active {
-      background-color: rgba(0,0,0,.1);
     }
   }
 }
