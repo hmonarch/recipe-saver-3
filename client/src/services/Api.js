cab7b639189b6be70a241d99f61a039867c229c5
doctafaustus <clowd150@gmail.com>
2019-05-14 10:37:14 -0500

    Configure baseURL for Axios

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export default function() {
 
-  const baseURL = (window.location.host !== rs3-staging.herokuapp.com) ? 'http://localhost:8081' : 'https://rs3-staging.herokuapp.com';
+  const baseURL = (window.location.host !== 'rs3-staging.herokuapp.com') ? 
+  'http://localhost:8081' : 'https://rs3-staging.herokuapp.com';
 
   return axios.create({
     baseURL
