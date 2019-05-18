bc4e6c135730fdab54676708465d5bf2408b0433
doctafaustus <clowd150@gmail.com>
2019-05-18 16:12:20 -0500

    Remove unneeded route

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
     <router-view/>
     <div class="mobile overlay" @click="closeMenus" :class="{ 'active': showOverlay === true }"></div>
   </div>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="home">
+
+    <br>
+      <a href="/login">Login</a>
+    <br>
+
+
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
