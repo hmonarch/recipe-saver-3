f0602b9c89afa61e541d1725c8626213e10bf079
doctafaustus <clowd150@gmail.com>
2019-09-02 23:09:26 -0500

    Finish extension interface, Connect to content script

--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,23 @@
 /* --- Set dev or prod --- */
-const mysite = 'https://localhost:3000/extension';
+const site = 'https://localhost:3000/extension';
 
-console.log('test');
+
+
+const moreEl = document.querySelector('#more');
+
+moreEl.addEventListener('click', () => {
+  moreEl.style.display = 'none';
+  document.querySelector('#extra-data').style.display = 'block';
+});
+
+
+
+// When popup.us opens, send message to content script to extract recipe data
+// On response, populate the extension form with the returned data
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {rsAction: 'extract'}, function(response) {
+
+    console.info('Response', response);
+
+  });
+});
