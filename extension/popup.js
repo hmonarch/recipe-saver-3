54dcb143cedbf2688c4103fbef2d38b7c5ef954b
doctafaustus <clowd150@gmail.com>
2019-11-14 10:12:40 -0600

    Retry extension upload after rejection

--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,6 @@
 // Set dev or prod site
 // const mode = 'prod';
-const mode = 'dev';
+const mode = 'prod';
 const site = (mode === 'prod') ? 'https://www.recipesaver.me' : 'https://localhost:8080';
 
 // Get whitelisted sites
