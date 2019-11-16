81f00f3b0b132820d2379da27a525dde818a69c1
doctafaustus <clowd150@gmail.com>
2019-11-16 12:20:53 -0600

    Add /plans redirect after Google or Facebook registration

--- app.js
+++ app.js
@@ -186,7 +186,7 @@ app.get('/auth/facebook/login/callback', passport.authenticate('facebook', { fai
   } else if (req.user.errMessage === 'Account already registered') {
     res.redirect('/login?login-reg-msg=fb-already-registered');
   } else {
-    res.redirect('/recipes');
+    res.redirect('/plans');
   }
 });
 
@@ -257,7 +257,7 @@ app.get(['/auth/google/login/callback', '/auth/google/register/callback'], passp
   } else if (req.user.errMessage === 'Email already registered') {
     res.redirect('/login?login-reg-msg=already-registered');
   } else {
-    res.redirect('/recipes');
+    res.redirect('/plans');
   }
 });
 
